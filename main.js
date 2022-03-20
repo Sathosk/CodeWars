64a55a8e423e1b88cc0cafc031d0946bb4e0f057
Tiago <sathoskath@gmail.com>
2022-03-20 04:28:12 -0300

    added challenge from 20-03-2022

--- main.js
+++ main.js
@@ -28,3 +28,35 @@ function greet(name){
     return "Hello, " + name + "!";
   }
 }
+
+//////// 20-03-2022
+/* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"*/
+
+/* function highAndLow(numbers){
+  let arrN = numbers.split(' ');
+  let highest = Number(arrN[0]);
+  let smallest = Number(arrN[0]);
+
+  for (i=0; i<arrN.length; i++) {
+    if (arrN[i] > highest) {
+      highest = Number(arrN[i]);
+    }
+
+    if (arrN[i] < smallest) {
+      smallest = Number(arrN[i]);
+    }
+    
+  }
+   return highest + ' ' + smallest;
+} */
+
+function highAndLow(numbers) {
+  numbers = numbers.split(' ').map(Number);
+  let orderedArray = numbers.sort((a, b) => a-b);
+  return orderedArray[orderedArray.length - 1] + ' ' + orderedArray[0];
+}
