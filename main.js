48cc323e8a9299d48f636851631b1ceaf130a21b
Tiago <sathoskath@gmail.com>
2022-03-21 02:16:59 -0300

    Added 21-03-2022 Challenges

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-///////// 18-03-2022
+///////// 18-03-2022 8kyu
 /* Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
 
 Examples
@@ -16,7 +16,7 @@ function powersOfTwo(n){
     return arr;
   }
 
-///////// 19-03-2022
+///////// 19-03-2022 8kyu
 /* Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
 
 Can you help her? */
@@ -29,7 +29,7 @@ function greet(name){
   }
 }
 
-//////// 20-03-2022
+//////// 20-03-2022 8kyu
 /* In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
 
 Examples
@@ -41,3 +41,56 @@ function highAndLow(numbers) {
   numbers = numbers.split(' ').sort((a, b) => a-b);
   return numbers[numbers.length - 1] + ' ' + numbers[0];
 }
+
+//////// 21-03-2022 8kyu
+//////// Even or Odd
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+
+//////// 21-03-2022 7kyu
+//////// Beginner Series #3 Sum of Numbers
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+function getSum(a, b) {
+  const smallestNumber = Math.min(a, b),
+        biggestNumber = Math.max(a, b);
+  let result = 0;
+
+  for (let i=smallestNumber; i<=biggestNumber; i++) {
+    result += i; 
+  }
+
+  return output;
+}
+
+//////// 21-03-2022 7kyu
+//////// Mumbling
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+	return [...s].map((element, i) => {
+    i++;
+    const str = element.repeat(i).toLowerCase();
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }).join('-');
+}
+
