82da65b87201b249092368ddad45fb3b417cbd5c
Tiago Cruz <sathoskath@gmail.com>
2022-04-03 16:08:38 -0300

    Added 03-04-2022 challenge

--- main.js
+++ main.js
@@ -195,3 +195,11 @@ function numberToString(num) {
   // return num.toString();
   return "" + num;
 }
+
+//////// 03-04-2022 8kyu
+//////// Convert a Number to a String!
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  return bool ? 'Yes' : 'No';
+}
