d21158261edfa68115d923cbfd676fc2fa9055ba
Tiago Cruz <sathoskath@gmail.com>
2022-04-03 14:23:52 -0300

    added 26-06-2022 challenges

--- main.js
+++ main.js
@@ -139,3 +139,44 @@ function solution(str){
    return arr.length === 0 ? arr = [] : arr.join('').split('.');
 }
 
+
+//////// 25-03-2022 6kyu
+//////// Is a number prime?
+// Define a function that takes one integer argument and returns logical value true or false depending on if the integer is a prime.
+
+// Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+// Requirements
+// You can assume you will be given an integer input.
+// You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+// NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
+// Example
+// is_prime(1)  /* false */
+// is_prime(2)  /* true  */
+// is_prime(-1) /* false */
+
+function isPrime(num) {
+  if (num < 2) return false;
+  const limit = Math.sqrt(num);
+  console.log(limit)
+  for (let i = 2; i <= limit; ++i) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//////// 26-03-2022 8kyu
+//////// Opposite number
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(num) {
+  return (-num);
+}
