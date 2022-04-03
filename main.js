4a9a56c97c4a032aa36168b7f6d2c4de39c3ab8c
Tiago Cruz <sathoskath@gmail.com>
2022-04-03 14:35:23 -0300

    Added 03-04-2022 challenge

--- main.js
+++ main.js
@@ -180,3 +180,18 @@ function isPrime(num) {
 function opposite(num) {
   return (-num);
 }
+
+//////// 03-04-2022 8kyu
+//////// Convert a Number to a String!
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+function numberToString(num) {
+  // return String(num);
+  // return num.toString();
+  return "" + num;
+}
