2f35d0742abf9448673e8da9dd0611a189e64d4d
Tiago Cruz <sathoskath@gmail.com>
2022-04-05 18:42:07 -0300

    Added 05-04-2022 Challenge

--- main.js
+++ main.js
@@ -226,3 +226,20 @@ function solution(str){
 function positiveSum(arr) {
   return arr.map(element => element > 0 ? element : 0).reduce((acc, curr) => acc += curr, 0);
 }
+
+function makeNegative(num) {
+  return num <= 0 ? num : -num;
+}
+
+//////// 05-04-2022 8kyu
+//////// Return Negative
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
