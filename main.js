e71e02ef770830fce057bf2aa1d4cbcc372e0a05
Tiago Cruz <sathoskath@gmail.com>
2022-04-17 06:28:23 -0300

    Added Challenges

--- main.js
+++ main.js
@@ -370,3 +370,45 @@ function countSheeps(arrayOfSheep) {
   arrayOfSheep.forEach(el => el ? i++ : i)
   return i;
 }
+
+//////// 15-04-2022 8kyu
+//////// Keep Hydrated!
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+function litres(time) {
+  return Math.floor(time*0.5);
+}
+
+//////// 16-04-2022 8kyu
+//////// Century From Year
+
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+function century(year) {
+  let yearDivided = year/100;
+  let roundedCentury = Math.floor(yearDivided);
+  return yearDivided > roundedCentury ? roundedCentury + 1 : roundedCentury;
+}
