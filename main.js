d54ce1d232634a3229cdef10600cf32f5c283a42
Tiago Cruz <sathoskath@gmail.com>
2022-04-23 08:07:15 -0300

    Added Challenge

--- main.js
+++ main.js
@@ -476,3 +476,20 @@ function isNarcissistic(n){
 function squareDigits(num){
   return +num.toString().split('').map(el => el ** 2).join('');
 }
+
+//////// 21-04-2022 7kyu
+//////// Abbreviate a Two Word Name
+
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+  return name.split(' ').map(el => el[0].toUpperCase()).join('.')
+}
