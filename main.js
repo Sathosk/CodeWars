f3fce85098267159c93398839eb2278b2e6dfcbc
Tiago Cruz <sathoskath@gmail.com>
2022-05-01 08:02:58 -0300

    Added Challenge

--- main.js
+++ main.js
@@ -589,3 +589,26 @@ function alphabetPosition(text) {
              .map(el => alphabet.indexOf(el.toLowerCase()) + 1)
              .join(' ')
 }
+
+//////// 27-04-2022 6kyu
+//////// Regex validate PIN code
+
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN (pin) {
+
+  return (pin.length !== 4 && pin.length !== 6) ? false : pin.search(/[^0-9]/g) === -1 ? true : false;
+
+  // if(pin.length !== 4 && pin.length !== 6) {
+  //   return false;
+  // } else {
+  //   return pin.search(/[^0-9]/g) === -1 ? true : false
+  // }
+}
