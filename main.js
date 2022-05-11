e6c934fa929e471940f944afbe4b707fb5ec646d
Tiago Cruz <sathoskath@gmail.com>
2022-05-11 13:32:45 -0300

    Added Challenges

--- main.js
+++ main.js
@@ -710,7 +710,7 @@ function paperwork(n, m) {
   return (n < 0) || (m < 0) ? 0 : n * m;
 }
 
-//////// 03-05-2022 8kyu
+//////// 03-05-2022 6kyu
 //////// Find the odd int
 
 // Given an array of integers, find the one that appears an odd number of times.
@@ -743,3 +743,13 @@ function findOdd(a) {
   })[0]
 }
 
+//////// 04-05-2022 8kyu
+//////// Opposites Attract
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  return (flower1+flower2) % 2 !== 0;
+}
