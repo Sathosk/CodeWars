5200ef7ffba301cd2612f5121c552fc3c699bce1
Tiago Cruz <sathoskath@gmail.com>
2022-05-12 14:13:44 -0300

    Added Challenges

--- main.js
+++ main.js
@@ -753,3 +753,18 @@ function findOdd(a) {
 function lovefunc(flower1, flower2){
   return (flower1+flower2) % 2 !== 0;
 }
+
+//////// 04-05-2022 7kyu
+//////// Reverse words
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+function reverseWords(str) {
+  return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
+}
+
+console.log(reverseWords('hi how Are u'))
