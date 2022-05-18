731e8c699e773cb9c3fdbc567c212645c77ef6c4
Tiago Cruz <sathoskath@gmail.com>
2022-05-18 20:59:21 -0300

    Added challenge

--- main.js
+++ main.js
@@ -831,3 +831,18 @@ function solution(number){
 function addBinary(a,b) {
   return (a+b).toString(2)
 }
+
+//////// 08-05-2022 7kyu
+//////// Reversed Sequence
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = n => {
+  let arr = [];
+  for (i=n; i>0; i--) {
+    arr.push(i)
+  };
+  return arr;
+};
