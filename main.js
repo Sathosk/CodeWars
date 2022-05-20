520b3f9c0556f7f8965133627bf13e83641d86f0
Tiago Cruz <sathoskath@gmail.com>
2022-05-19 23:50:45 -0300

    Added Challenge

--- main.js
+++ main.js
@@ -846,3 +846,33 @@ const reverseSeq = n => {
   };
   return arr;
 };
+
+//////// 09-05-2022 7kyu
+//////// Remove the minimum
+
+// The museum of incredible dull things
+// The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output= [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+function removeSmallest(numbers) {
+  let newArr = numbers;
+  newArr.splice(numbers.indexOf(Math.min.apply(null, numbers)), 1);
+  return newArr;
+}
+
+let array = [2,2,1,2,1];
+
+
+console.log(removeSmallest(array))
+console.log(array)
