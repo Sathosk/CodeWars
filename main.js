f0ba7b023db03ef09c1180b4e2118919629ae8f7
Tiago Cruz <sathoskath@gmail.com>
2022-05-22 23:54:40 -0300

    Added Challenge

--- main.js
+++ main.js
@@ -916,4 +916,29 @@ function plural(n) {
   return n !== 1;
 }
 
-console.log(plural(0.5))
+
+//////// 11-05-2022 8kyu
+//////// Sum without highest and lowest number
+
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0
+
+function sumArray(array) {
+  return !array || !array.length === 1 ? 0 : array.map((el, i, array) => {
+    el !== Math.min.apply(null, array) ? el : array.indexOf(el)
+  });
+}
+
+console.log(sumArray([6,2,1,8,10]))
+
+
