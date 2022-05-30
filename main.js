d92b8e32ecf7cc3b43a67ad230ebad5c12dc1473
Tiago Cruz <sathoskath@gmail.com>
2022-05-30 03:40:13 -0300

    Added Challenge

--- main.js
+++ main.js
@@ -1026,3 +1026,26 @@ function isPangram(string){
 
   return newArr.length === 26
 }
+
+//////// 17-05-2022 7kyu
+//////// Ghostbusters (whitespace removal)
+
+// Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+// In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+// So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+// Example:
+
+// ghostBusters("Sky scra per");
+// Should return:
+
+// "Skyscraper"
+// If the building contains no ghosts, return the string:
+
+// "You just wanted my autograph didn't you?"
+
+function ghostBusters(building) {
+  return building.includes(' ') ? building.replace(/ /g, '') : "You just wanted my autograph didn't you?";
+}
