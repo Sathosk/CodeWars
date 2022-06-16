8b821d1c710c3ae2947ccf303b4edc011a0b1d03
Tiago Cruz <sathoskath@gmail.com>
2022-06-15 22:58:30 -0300

    added challenges

--- main.js
+++ main.js
@@ -1281,3 +1281,19 @@ function likes(names) {
     default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
   }
 }
+
+//////// 24-05-2022 8kyu
+//////// Are You Playing Banjo?
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+function areYouPlayingBanjo(name) {
+  return name[0].toLowerCase() === 'r' ? name + ' plays banjo' : name + ' does not play banjo'
+}
