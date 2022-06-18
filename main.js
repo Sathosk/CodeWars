0c03c14d4c452338e46c06231a8651c1cfabe8b0
Tiago Cruz <sathoskath@gmail.com>
2022-06-17 22:46:24 -0300

    added challenge

--- main.js
+++ main.js
@@ -1310,7 +1310,24 @@ function areYouPlayingBanjo(name) {
 
 function solution(string) {
   return(string.replace(/([A-Z])/g, ' $1'));
-
 }
 
-console.log(solution('camelCasingOi'))
+//////// 26-05-2022 6kyu
+//////// Unique In Order
+
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder=function(iterable){
+  if (typeof iterable === 'string') {iterable = iterable.split('')}
+  return iterable.filter((element, i, array) => {
+    return array[i+1] === element ? false : true;
+  })
+}
+
+
