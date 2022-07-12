460c923fb71818553f3325f869fa6d31614578a6
Tiago Cruz <sathoskath@gmail.com>
2022-07-12 07:36:29 -0300

    added challenge

--- main.js
+++ main.js
@@ -1721,3 +1721,35 @@ function between(a, b) {
 
   return arr;
 }
+
+//////// 12-07-2022 6kyu
+//////// Write Number in Expanded Form
+
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+function expandedForm(num) {
+  let currentN = String(num);
+  if (currentN.length === 1) return currentN;
+
+  let expandedForm = '';
+
+  while (currentN.length > 1) {
+    let divisor = '1' + '0'.repeat(currentN.length-1);
+    let remainder = +currentN % +divisor;
+    let decimal = +currentN - remainder;
+    
+    String(remainder).length > 1 ? 
+    expandedForm += `${decimal} + `: 
+    expandedForm += `${decimal}`;
+
+    currentN = String(remainder);
+  }
+
+  return currentN === '0' ? expandedForm : expandedForm + ` + ${currentN}`;
+}
