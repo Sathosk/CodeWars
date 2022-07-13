0138e8e47a0c5c263c59fa7072c08c85ff43fb93
Tiago Cruz <sathoskath@gmail.com>
2022-07-13 18:48:55 -0300

    added challenge

--- main.js
+++ main.js
@@ -1753,3 +1753,21 @@ function expandedForm(num) {
 
   return currentN === '0' ? expandedForm : expandedForm + ` + ${currentN}`;
 }
+
+//////// 13-07-2022 6kyu
+//////// How good are you really?
+
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return True if you're better, else False!
+
+// Note:
+// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+function betterThanAverage(classPoints, yourPoints) {
+  return classPoints.reduce((acc, cur) => cur + acc) / classPoints.length < yourPoints;
+}
+
