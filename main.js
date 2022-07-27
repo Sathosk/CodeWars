4755370a59687ed2689b7351fc2fa0bdb74fb4de
Tiago Cruz <sathoskath@gmail.com>
2022-07-27 07:50:34 -0300

    added challenge

--- main.js
+++ main.js
@@ -2065,3 +2065,21 @@ function doubleInteger(i) {
   // i will be an integer. Double it and return it.
   return i*2;
 }
+
+//////// 27-07-2022 6kyu
+//////// Bit Counting
+
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+const countBits = function(n) {
+  let binary = n.toString(2);
+  let count = 0;
+
+  for (let i=0; i<binary.length; i++) {
+    if (binary[i] == 1) {count++};
+  }
+
+  return count;
+};
