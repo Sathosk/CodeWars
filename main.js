5f31f3fdb21855b370d9bafaed332f92df91944f
Tiago Cruz <sathoskath@gmail.com>
2022-07-28 16:31:39 -0300

    added challenge

--- main.js
+++ main.js
@@ -2083,3 +2083,14 @@ const countBits = function(n) {
 
   return count;
 };
+
+//////// 28-07-2022 8kyu
+//////// Bit Counting
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(x){
+  return x.reduce((a, b) => +a + +b, 0);
+}
