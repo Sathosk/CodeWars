0ecac085aa3df6002f7e401db0cff47a3a735ce7
Tiago Cruz <sathoskath@gmail.com>
2022-08-13 22:42:20 -0300

    added challenge

--- main.js
+++ main.js
@@ -2413,8 +2413,6 @@ function countBy(x, n) {
   return z;
 }
 
-console.log(countBy(2,5))
-
 //////// 09-08-2022 8kyu
 //////// Convert number to reversed array of digits
 
@@ -2476,3 +2474,30 @@ String.prototype.isUpperCase = function() {
   return this.toString() === this.toUpperCase();
 }
 
+//////// 13-08-2022 7kyu
+//////// Sum of a sequence
+
+// Your task is to make function, which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+// If begin value is greater than the end, function should returns 0
+
+// Examples
+
+// 2,2,2 --> 2
+// 2,6,2 --> 12 (2 + 4 + 6)
+// 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+// 1,5,3  --> 5 (1 + 4)
+
+const sequenceSum = (begin, end, step) => {
+  let res = 0;
+
+  for (let i=begin; i<=end; i+=step) {
+    res += i;
+  }
+
+  return res;
+};
+
+console.log(sequenceSum(2,6,2))
