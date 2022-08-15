240ba7f9b8cca639955fd412bb3f99d57ad122f4
Tiago Cruz <sathoskath@gmail.com>
2022-08-14 23:23:49 -0300

    added challenge

--- main.js
+++ main.js
@@ -2501,3 +2501,21 @@ const sequenceSum = (begin, end, step) => {
 };
 
 console.log(sequenceSum(2,6,2))
+
+//////// 14-08-2022 7kyu
+//////// Execute me nTimes
+
+// You're given a procedure / method that executes a passed action n times, but it does so in a purely sequential manner. If you inspect the sample tests, you'll find that the action takes about 1 second to complete, and this action is repeated 20 times which results in a timeout.
+
+// Find a way to execute the action n times in parallel, such that the sample tests complete under the 12 second time limit imposed by Codewars. The submission tests will also test for similar scenarios.
+
+// Hint: If you're not sure where to start, read up on async/await.
+
+
+async function execute(action, nTimes){
+  const arr = [];
+  for(let i = 0; i < nTimes; i++){
+    arr.push(action());
+  }
+  await Promise.all(arr);
+}
