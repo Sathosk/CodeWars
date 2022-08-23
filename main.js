84ece0be6b5bf8dcc4b2c5abb57035d8b9a0e482
Tiago Cruz <sathoskath@gmail.com>
2022-08-23 20:57:29 -0300

    added challenge

--- main.js
+++ main.js
@@ -2759,3 +2759,15 @@ function moveZeros(arr) {
 
   return noZeroArr.concat(zeroArr);
 }
+
+//////// 23-08-2022 7kyu
+//////// Small enough? - Beginner
+
+// You will be given an array and a limit value. 
+// You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+function smallEnough(a, limit){
+  return a.every(n => n <= limit);
+}
