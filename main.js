25217f0150ad56ef81a27b06a8479249f39f7de8
Tiago Cruz <sathoskath@gmail.com>
2022-08-27 16:30:13 -0300

    added challenge

--- main.js
+++ main.js
@@ -2771,3 +2771,21 @@ function moveZeros(arr) {
 function smallEnough(a, limit){
   return a.every(n => n <= limit);
 }
+
+//////// 27-08-2022 7kyu
+//////// Build a square
+
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+
+// +++
+// +++
+// +++
+
+function generateShape(integer){
+  let str = '+'.repeat(integer);
+
+  return integer > 1 ? str+`\n${str}`.repeat(integer-1) : str;
+}
