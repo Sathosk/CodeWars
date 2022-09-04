b2a0352876976e5803aa8ca91dcbe61bd5653d0d
Tiago Cruz <sathoskath@gmail.com>
2022-09-03 21:27:00 -0300

    added challenge

--- main.js
+++ main.js
@@ -2869,7 +2869,7 @@ function compareVersions (version1, version2) {
   return true;
 }
 
-//////// 02-09-2022 7kyu
+//////// 02-09-2022 6kyu
 //////// Where my anagrams at?
 
 // What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
@@ -2899,3 +2899,34 @@ function anagrams(word, words) {
   })
 }
 
+//////// 03-09-2022 6kyu
+//////// Which are in?
+
+// Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+
+// Example 1:
+// a1 = ["arp", "live", "strong"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns ["arp", "live", "strong"]
+
+// Example 2:
+// a1 = ["tarp", "mice", "bull"]
+
+// a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+// returns []
+
+// Notes:
+// Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+// In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+// Beware: r must be without duplicates.
+
+function inArray(array1,array2){
+  return array1.filter(word => {
+    for (let i=0; i<array2.length; i++) {
+      if (array2[i].includes(word)) return true;
+    }
+  }).sort();
+}
