1199acfa385ce479d42c0f14b38bfb869c92d1d3
Tiago Cruz <sathoskath@gmail.com>
2022-09-07 16:55:50 -0300

    added challenge

--- main.js
+++ main.js
@@ -2930,3 +2930,16 @@ function inArray(array1,array2){
     }
   }).sort();
 }
+
+//////// 07-09-2022 7kyu
+//////// Remove anchor from URL
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+  return url.replace(/[^#]*$/, '');
+}
