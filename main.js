91da2aca2ab8113faab140dc454ec4b8f40ed606
Tiago Cruz <sathoskath@gmail.com>
2022-09-11 23:47:26 -0300

    added challenge

--- main.js
+++ main.js
@@ -3004,3 +3004,18 @@ function nextVersion(version){
 
   return verArr.join('.');
 }
+
+//////// 11-09-2022 7kyu
+//////// Reversed Words
+
+// DESCRIPTION:
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
