2ea924894cdadbf81a04e5e81d266733b8f97863
Tiago Cruz <sathoskath@gmail.com>
2022-09-17 23:13:42 -0300

    added challenge

--- main.js
+++ main.js
@@ -3092,5 +3092,33 @@ function giveChange(amount) {
   return answerArr;
 }
 
-//////// 16-09-2022 7kyu
-//////// 
+//////// 17-09-2022 7kyu
+//////// Stanton measure
+
+// The Stanton measure of an array is computed as follows: count the number of occurences for value 1 in the array. 
+// Let this count be n. The Stanton measure is the number of times that n appears in the array.
+
+// Write a function which takes an integer array and returns its Stanton measure.
+
+// Examples
+// The Stanton measure of [1, 4, 3, 2, 1, 2, 3, 2] is 3, because 1 occurs 2 times in the array and 2 occurs 3 times.
+
+// The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times in the array and 3 occurs 1 time.
+
+function stantonMeasure(array) {
+  let newArr = array.slice();
+  let n = 0;
+  let stantonMeasure = 0;
+
+  for (let i=0; i<newArr.length; i++) {
+    if (newArr[i] === 1) {
+      n++;
+      newArr.splice(i, 1);
+      i--;
+    }
+  }
+
+  return newArr.reduce((counter, curr) => curr == n ? ++counter : counter, 0)
+}
+
+console.log(stantonMeasure([-5, 1]))
