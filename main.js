7a4c238cabd2ccd26a83fbb734134e242942253d
Tiago Cruz <sathoskath@gmail.com>
2022-10-06 16:20:18 -0300

    added challenge

--- main.js
+++ main.js
@@ -3182,4 +3182,27 @@ function reverse(str){
   return arr.join(' ');
 }
 
-console.log(reverse(' oi como vai voce?'))
+//////// 06-10-2022 7kyu
+//////// Filter the number
+
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+// Task
+// Your task is to return a number from a string.
+
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+const filterString = function(str) {
+  let n = '';
+  for (let i=0; i<str.length; i++) {
+    if (str[i].match(/[0-9]/)) {
+      n+=str[i];
+    }
+  }
+
+  return +n;
+}
+
+console.log(filterString('1sd2sdf4'))
