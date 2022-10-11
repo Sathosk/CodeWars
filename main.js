da494e4479821499cdce2b6fa3ce5d733956058b
Tiago Cruz <sathoskath@gmail.com>
2022-10-11 19:57:05 -0300

    added challenges

--- main.js
+++ main.js
@@ -3298,3 +3298,32 @@ function points(games) {
 
   return score;
 }
+
+//////// 10-10-2022 8kyu
+//////// Convert string to camel case
+
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+// The first word within the output should be capitalized only if the original word was capitalized 
+// (known as Upper Camel Case, also often referred to as Pascal case).
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+// function toCamelCase(str){
+//   let specialChar = str.match(/[-_]/);
+
+//   while (specialChar) {
+//     str = str.slice(0, str.indexOf(specialChar)) + 
+//           str[str.indexOf(specialChar) + 1].toUpperCase() + 
+//           str.slice(str.indexOf(specialChar) + 2, str.length);
+
+//     specialChar = str.match(/[-_]/i);
+//   }
+  
+//   return str;
+// }
+
+function toCamelCase(str){
+  return str.replace(/[-_](.)/g, (match, c) => c.toUpperCase())
+}
