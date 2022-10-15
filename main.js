2864e6ddf80b1cd3b7e8b21db681c3f0b83d2f83
Tiago Cruz <sathoskath@gmail.com>
2022-10-14 22:56:30 -0300

    added challenge

--- main.js
+++ main.js
@@ -3347,3 +3347,49 @@ function search(budget, prices) {
                  .sort((a, b) => a - b)
                  .join(',');
 }
+
+//////// 14-10-2022 7kyu
+//////// Unusual Lex Order
+
+// The citizens of Codeland read each word from right to left, meaning that lexicographical comparison works differently in their language. 
+// Namely, string a is lexicographically smaller than string b if either: a is a suffix of b (in common sense, i.e. b ends with a substring equal to a); 
+// or their last k characters are the same but the (k + 1)th character from the right in string a is smaller than the same character in string b.
+
+// Given an array of words in Codeland language, sort them lexicographically according to Codeland's unique rules.
+
+// For words = ["nigeb", "ta", "eht", "gninnigeb"], the output should be unusualLexOrder(words) = ["ta", "nigeb", "gninnigeb", "eht"].
+
+// In particular, "ta" < "nigeb" because 'a' < 'b' and "nigeb" < "gninnigeb" because the former word is a suffix of the latter.
+
+function unusualLexOrder(arr){
+  let newArr = arr.map(word => word.split('').reverse().join(''))
+
+  newArr.sort((a, b) => {
+
+    if (a < b) {
+      return -1;
+    }
+    
+    else if (a < b) {
+      return 0;
+    }
+    
+    else {
+      return 1;
+    }
+  })
+
+  return newArr.map(word => word.split('').reverse().join(''))
+}
+
+console.log(unusualLexOrder([ 'osqd',
+'jivapwnek',
+'qp',
+'yfq',
+'q',
+'szfus',
+'jvqtsyct',
+'aysmlu',
+'u',
+'cx' ]))
+
