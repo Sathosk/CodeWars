d2cacad3737ce8a5f279f23d85ae982d7732752b
Tiago Cruz <sathoskath@gmail.com>
2022-10-16 21:34:50 -0300

    added challenge

--- main.js
+++ main.js
@@ -3382,14 +3382,37 @@ function unusualLexOrder(arr){
   return newArr.map(word => word.split('').reverse().join(''))
 }
 
-console.log(unusualLexOrder([ 'osqd',
-'jivapwnek',
-'qp',
-'yfq',
-'q',
-'szfus',
-'jvqtsyct',
-'aysmlu',
-'u',
-'cx' ]))
+//////// 16-10-2022 7kyu
+//////// 254 shades of grey
 
+// Why would we want to stop to only 50 shades of grey? Let's see to how many we can go.
+
+// Write a function that takes a number n as a parameter and return an array containing n shades of grey in hexadecimal code (#aaaaaa for example). 
+// The array should be sorted in ascending order starting with '#010101', '#020202', etc. (using lower case letters).
+
+// Examples:
+
+// n =  1:    ["#010101"]
+// n = 10:    ["#010101", "#020202", "#030303", "#040404", "#050505", "#060606", "#070707", "#080808", "#090909", "#0a0a0a"]
+// As a reminder, the grey color is composed by the same number of red, green and blue: #010101, #aeaeae, or #555555.
+
+// Black: #000000 and white: #ffffff are not accepted values.
+
+// When n is negative, just return an empty array. If n is higher than 254, just return an array of 254 elements.
+
+// Have fun!
+
+function shadesOfGrey(n){
+  if (n < 1) return [];
+  if (n > 254) n = 254;
+  let arrayOfShades = [];
+  let hexaDecimalArray = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+
+  for (let i=1; i<=Math.min(n, 254); i++) {
+    let firstDigit = hexaDecimalArray[Math.floor(i/16)];
+    let secondDigit = hexaDecimalArray[i%16];
+    arrayOfShades.push('#' + (firstDigit + secondDigit).repeat(3));
+  }
+
+  return arrayOfShades;
+}
