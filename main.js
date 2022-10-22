a1210739bf3b4a1eee3d6f193f50801ca4518c6b
Tiago Cruz <sathoskath@gmail.com>
2022-10-21 21:16:57 -0300

    added challenge

--- main.js
+++ main.js
@@ -3504,3 +3504,33 @@ const generateContinuedFraction = function* (n){
     while (q == 1n) yield 0n; 
   }
 }
+
+//////// 21-10-2022 6kyu
+//////// +1 Array
+
+// Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+// the array can't be empty
+// only non-negative, single digit integers are allowed
+// Return nil (or your language's equivalent) for invalid inputs.
+
+// Examples
+// For example the array [2, 3, 9] equals 239, adding one would return the array [2, 4, 0].
+
+// [4, 3, 2, 5] would return [4, 3, 2, 6]
+
+function upArray(arr) {
+  if (arr.length == 0 || arr.some(e => e < 0 || e > 9)) return null
+  
+  let i = arr.length - 1
+  
+  while (i >= 0 && arr[i] == 9)
+    arr[i--] = 0
+  
+  if (i < 0)
+    arr.unshift(1)
+  else
+    arr[i]++
+  
+  return arr
+}
