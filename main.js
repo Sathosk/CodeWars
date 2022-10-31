cde082e47c2903ea9ad7dca34eddc416f086d9c2
Tiago Cruz <sathoskath@gmail.com>
2022-10-31 13:00:54 -0300

    added challenge

--- main.js
+++ main.js
@@ -3643,3 +3643,30 @@ function incrementer(nums) {
 function sumStrings(a,b) { 
   return BigInt(BigInt(a) + BigInt(b)).toString();
 }
+
+//////// 31-10-2022 8kyu
+//////// Basic Mathematical Operations
+
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// Examples(Operator, value1, value2) --> output
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+    case "-":
+      return value1 - value2;
+    case "*":
+      return value1 * value2;
+    case "/":
+      return value1 / value2;
+  }
+}
