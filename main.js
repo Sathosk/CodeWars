5bf685391f4d6b0631e9f4f4e3c1f393d817ba07
Tiago Cruz <sathoskath@gmail.com>
2022-11-02 13:21:56 -0300

    added challenge

--- main.js
+++ main.js
@@ -3688,3 +3688,29 @@ function shorter_reverse_longer(a,b){
   
   return a + b.split('').reverse().join('') + a;
 }
+
+//////// 02-11-2022 7kyu
+//////// Vowel one
+
+// vowelOne
+// Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+
+// All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+
+// Examples:
+
+// vowelOne( "abceios" ) // "1001110"
+
+// vowelOne( "aeiou, abc" ) // "1111100100"
+
+function vowelOne(s){
+  let vowel = ['a', 'e', 'i', 'o', 'u'];
+  let splitStr = s.split('');
+  let result = '';
+
+  for (let i=0; i<splitStr.length; i++) {
+    vowel.includes(splitStr[i].toLowerCase()) ? result += '1' : result += '0';
+  }
+
+  return result;
+}
