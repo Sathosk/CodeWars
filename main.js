c4e722b070dfeccd1f12104b88cbb3912371a96c
Tiago Cruz <sathoskath@gmail.com>
2022-11-30 21:54:37 -0300

    added challenge

--- main.js
+++ main.js
@@ -3940,3 +3940,33 @@ function longestRepetition(s) {
 
   return [character, highestCount];
 }
+
+//////// 30-11-2022 6kyu
+//////// Create Phone Number
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+// The returned format must be correct in order to complete this challenge.
+
+// Don't forget the space after the closing parentheses!
+
+function createPhoneNumber(numbers){
+  let areaCode = '';
+  let phoneNumber = '';
+
+  for (let i=0; i<numbers.length; i++) {
+    if (i < 3) {
+      areaCode += numbers[i];
+    } else {
+      if (i == 6) {
+        phoneNumber += '-'
+      }
+      phoneNumber += numbers[i];
+    }
+  }
+
+  return `(${areaCode}) ${phoneNumber}`;
+}
+
+console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]))
