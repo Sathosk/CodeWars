578c38d26a8df77ff1d3cf6bffe94e4464e929aa
Tiago Cruz <sathoskath@gmail.com>
2022-12-07 22:55:52 -0300

    added challenge

--- main.js
+++ main.js
@@ -4101,3 +4101,17 @@ function lastSurvivor(letters, coords) {
   
   return letters.join('')
 }
+
+//////// 07-12-2022 5kyu
+//////// Invert values
+
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+function invert(array) {
+  return array.map(n => -n)
+}
