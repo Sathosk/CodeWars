987a5b3d13e7f16592e32face684d8a41d56cbd5
Tiago Cruz <sathoskath@gmail.com>
2022-12-10 20:45:58 -0300

    added challenge

--- main.js
+++ main.js
@@ -4117,4 +4117,15 @@ function invert(array) {
 }
 
 //////// 09-12-2022 5kyu
-//////// 
+//////// Perpendicular lines
+
+// You are given an input (n) which represents the amount of lines you are given, 
+// your job is to figure out what is the maximum amount of perpendicular bisectors you can make using these lines.
+
+// Note: A perpendicular bisector is one that forms a 90 degree angle
+
+// n will always be greater than or equal to 0
+
+function maxBisectors(n) {
+  return Math.floor(n * n / 4);
+}
