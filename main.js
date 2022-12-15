3c548af82042a458ca7ddabb5ebdfa422937b56c
Tiago Cruz <sathoskath@gmail.com>
2022-12-15 17:47:53 -0300

    added challenge

--- main.js
+++ main.js
@@ -4186,3 +4186,35 @@ function isValidChess960 (pieces) {
 
 	return true;
 }
+
+//////// 15-12-2022 7kyu
+//////// Complementary DNA
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" 
+// for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+// Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. 
+// DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+function DNAStrand(dna){
+  const basePairing = { A: 'T', T: 'A', C: 'G', G: 'C' }
+  let complementarySide = '';
+
+  for (let i=0; i<dna.length; i++) {
+    complementarySide += basePairing[dna[i]];
+  }
+
+  return complementarySide;
+}
+
+console.log(DNAStrand('ATTGC'))
