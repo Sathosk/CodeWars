4cc3e343cfb82c69168a8151112f4e4ec590c466
Tiago Cruz <sathoskath@gmail.com>
2022-12-21 19:09:49 -0300

    added challenge

--- main.js
+++ main.js
@@ -4271,3 +4271,26 @@ const prevMultOfThree = n => {
 function SubtractSum(n){
   return 'apple';
 }
+
+//////// 21-12-2022 6kyu
+//////// N-th Fibonacci
+
+// I love Fibonacci numbers in general, but I must admit I love some more than others.
+
+// I would like for you to write me a function that when given a number (n) returns the n-th number in the Fibonacci Sequence.
+
+// For example:
+
+//    nthFibo(4) == 2
+// Because 2 is the 4th number in the Fibonacci Sequence.
+
+// For reference, the first two numbers in the Fibonacci sequence are 0 and 1,
+// and each subsequent number is the sum of the previous two.
+
+function nthFibo(n) {
+  let currentN = [0, 1];
+
+  for (let i=2; i<n; i++) currentN.push(currentN[i-1] + currentN[i-2]);
+
+  return currentN[n-1];
+}
