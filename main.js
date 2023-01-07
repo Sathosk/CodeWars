5f40085488c9c43e10f0aa2c0f9f2071e5b08725
Tiago <sathoskath@gmail.com>
2023-01-07 19:23:16 -0300

    added challenge

--- main.js
+++ main.js
@@ -4498,3 +4498,35 @@ function consonantCount(str) {
 
   return res;
 }
+
+//////// 07-01-2023 7kyu
+//////// Collatz Conjecture Length
+
+// The Collatz Conjecture states that for any positive natural number n, this process:
+
+// if n is even, divide it by 2
+// if n is odd, multiply it by 3 and add 1
+// repeat
+// will eventually reach n = 1.
+
+// For example, if n = 20, the resulting sequence will be:
+
+// [ 20, 10, 5, 16, 8, 4, 2, 1 ]
+// Write a program that will output the length of the Collatz Conjecture for any given n.
+// In the example above, the output would be 8.
+
+function collatz(n) {
+  let resultArr = [n];
+
+  while (n > 1) {
+    if (n % 2 === 0) {
+      resultArr.push(n / 2);
+      n = n / 2;
+    } else {
+      resultArr.push((n * 3) + 1);
+      n = (n * 3) + 1;
+    }
+  }
+
+  return resultArr.length;
+}
