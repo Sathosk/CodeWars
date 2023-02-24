9d21b0be2c441cd920cba5a03159f98692b0fcd2
Tiago C <sathoskath@gmail.com>
2023-02-24 18:08:33 +0800

    added challenge

--- main.js
+++ main.js
@@ -4864,3 +4864,33 @@ function sumNoDuplicates(numList) {
 
   return sum;
 }
+
+//////// 24-02-2023 7kyu
+//////// Pair of gloves
+
+// Pair of gloves
+// Winter is coming, you must prepare your ski holidays. 
+// The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+
+// Given an array describing the color of each glove, return the number of pairs you can constitute,
+// assuming that only gloves of the same color can form pairs.
+
+// Examples:
+// input = ["red", "green", "red", "blue", "blue"]
+// result = 2 (1 red pair + 1 blue pair)
+
+// input = ["red", "red", "red", "red", "red", "red"]
+// result = 3 (3 red pairs)
+
+function numberOfPairs(gloves) {
+  let glovesObj = {};
+
+  for (let glove of gloves) {
+    glovesObj[glove] ? glovesObj[glove] = glovesObj[glove] + 1 : glovesObj[glove] = 1;
+  }
+  
+  return Object.values(glovesObj)
+               .reduce((acc, curr) => acc + Math.floor(curr / 2), 0)
+}
+
+console.log(numberOfPairs(['gray','black','purple','purple','gray','black']))
