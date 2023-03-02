b03816d94b4758d176aa7549a7f88f095d23e36e
Tiago C <sathoskath@gmail.com>
2023-03-02 17:22:07 +0800

    added challenge

--- main.js
+++ main.js
@@ -4987,3 +4987,24 @@ function kaCokadekaMe(word) {
 
   return 'ka' + word.replace(/([aeiou]+)([^aeiou])/ig,'$1ka$2');
 }
+
+//////// 02-03-2023 6kyu
+//////// Sort by Last Char
+
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+function last(x){
+  let arr = x.split(' ');
+
+  return arr.sort((a, b) => {
+    if (a[a.length - 1] < b[b.length - 1]) return -1;
+    if (a[a.length - 1] > b[b.length - 1]) return 1;
+    return 0;
+  });
+}
+
+console.log(last('man i need a taxi up to ubud'))
