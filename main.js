e76a67e611df12250b72b1f8f16c985b39016b0a
Tiago C <sathoskath@gmail.com>
2023-03-08 15:57:01 +0800

    added challenge

--- main.js
+++ main.js
@@ -5036,3 +5036,33 @@ class Cat extends Animal {
     return `${this.name} meows.`;
   }
 }
+
+//////// 08-03-2023 7kyu
+//////// Merge overlapping strings
+
+// This kata requires you to write a function which merges two strings together. 
+// It does so by merging the end of the first string with the start of the second string together when they are an exact match.
+
+// "abcde" + "cdefgh" => "abcdefgh"
+// "abaab" + "aabab" => "abaabab"
+// "abc" + "def" => "abcdef"
+// "abc" + "abc" => "abc"
+// NOTE: The algorithm should always use the longest possible overlap.
+
+// "abaabaab" + "aabaabab" would be "abaabaabab" and not "abaabaabaabab"
+
+function mergeStrings(first, second){
+  let count = 0;
+  
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] == second[count]) {
+      count++;
+    } else {
+      count = 0;
+      if (first[i] == second[count]) count++;
+    }
+  }
+
+  return first + second.slice(count);
+}
+
