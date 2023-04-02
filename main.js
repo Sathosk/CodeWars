54f675329376252dbd347bf829bbac2355cb78e9
Tiago C <sathoskath@gmail.com>
2023-04-02 10:09:54 +0800

    added challenge

--- main.js
+++ main.js
@@ -5423,3 +5423,27 @@ function trim(str, size) {
   if (size <= 3) return str.substring(0, size) + '...';
   return str.substring(0, size - 3) + '...'
 }
+
+//////// 02-04-2023 7kyu
+//////// Keep the Order
+
+// Your job here is to write a function (keepOrder in JS/CoffeeScript, keep_order in Ruby/Crystal/Python, keeporder in Julia), 
+// which takes a sorted array ary and a value val, and returns the lowest index where you could insert val to maintain the sorted-ness of the array. 
+// The input array will always be sorted in ascending order. It may contain duplicates.
+
+// Do not modify the input.
+
+// Some examples:
+// keepOrder([1, 2, 3, 4, 7], 5) //=> 4
+//                       ^(index 4)
+// keepOrder([1, 2, 3, 4, 7], 0) //=> 0
+//           ^(index 0)
+// keepOrder([1, 1, 2, 2, 2], 2) //=> 2
+//                 ^(index 2)
+
+function keepOrder(ary, val) {
+  let newArr = [...ary, val];
+  newArr.sort();
+
+  return newArr.indexOf(val);
+}
