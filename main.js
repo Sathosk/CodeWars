94d7d4bbfc6f82f6a9a7843c79247e00191907e9
Tiago C <sathoskath@gmail.com>
2023-04-09 23:32:15 +0800

    added challenge

--- main.js
+++ main.js
@@ -5541,3 +5541,20 @@ String.prototype.fromBase64 = function() {
   const buf = Buffer.from(this, 'base64');
   return buf.toString();
 }
+
+//////// 09-04-2023 7kyu
+//////// Sum even numbers
+
+// Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+
+// Only numbers without decimals like 4 or 4.0 can be even.
+
+// The input is a sequence of numbers: integers and/or floats.
+
+// Examples
+// [4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
+// []                               -->  0
+
+function sumEvenNumbers(input) {
+  return input.reduce((acc, curr) => curr % 2 === 0 ? acc + curr : acc)
+}
