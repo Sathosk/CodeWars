cc26199d0ef3671554483dea4d32201a825de7c5
Tiago C <sathoskath@gmail.com>
2023-04-13 21:33:18 +0800

    added challenge

--- main.js
+++ main.js
@@ -5638,3 +5638,19 @@ function maximizePoints(team1, team2) {
 
   return j;
 }
+
+//////// 13-04-2023 8kyu
+//////// Simple Fun #210: Maximize Points
+
+// All of the animals are having a feast! Each animal is bringing one dish. 
+// There is just one rule: the dish must start and end with the same letters as the animal's name. 
+// For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+
+// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+
+// Assume that beast and dish are always lowercase strings, and that each has at least two letters. 
+// beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+
+function feast(beast, dish) {
+  return dish[0] === beast[0] && dish[dish.length - 1] === beast[beast.length - 1];
+}
