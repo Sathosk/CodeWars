dc1ead825bf23e1bcd58e27234798afe1c378015
Tiago <sathoskath@gmail.com>
2023-06-28 23:46:25 -0300

    added challenge

--- main.js
+++ main.js
@@ -5839,3 +5839,20 @@ function sumMul(n, m){
 
   return sum;
 }
+
+//////// 26-06-2023 7kyu
+//////// Leap Years
+
+// In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+// years divisible by 4 are leap years
+// but years divisible by 100 are not leap years
+// but years divisible by 400 are leap years
+// Additional Notes:
+
+// Only valid years (positive integers) will be tested, so you don't have to validate them
+// Examples can be found in the test fixture.
+
+function isLeapYear(year) {
+  return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
+}
