b07f1a17828b1f14b0992b99308a0b835edf0c27
Tiago <sathoskath@gmail.com>
2023-08-01 07:27:53 -0300

    added challenge

--- main.js
+++ main.js
@@ -5856,3 +5856,31 @@ function sumMul(n, m){
 function isLeapYear(year) {
   return (year % 100 !== 0 && year % 4 === 0) || year % 400 === 0;
 }
+
+//////// 01-08-2023 7kyu
+//////// Triangular Treasure
+
+// Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+// 1st (1)   2nd (3)    3rd (6)
+// *          **        ***
+//            *         **
+//                      *
+// You need to return the nth triangular number. You should return 0 for out of range values:
+
+// For example: (Input --> Output)
+
+// 0 --> 0
+// 2 --> 3
+// 3 --> 6
+// -10 --> 0
+
+
+
+function triangular(n) {
+  const max_n = Math.floor(Math.sqrt(Number.MAX_VALUE));
+
+  if (n < 1 || n >= max_n) return 0;
+  
+  return (n * (n + 1)) / 2;
+}
