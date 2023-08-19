32dee9fbe90c6a5d4d6cc44d86595dfeb4867e82
Tiago <sathoskath@gmail.com>
2023-08-18 21:21:17 -0300

    added challenge

--- main.js
+++ main.js
@@ -6018,3 +6018,25 @@ function to24hourtime(hour, minute, period) {
   }
   return hour.toString().padStart(2, "0") + minute.toString().padStart(2, "0");
 }
+
+//////// 18-08-2023 7kyu
+//////// Selective fear of numbers
+
+// I've got a crazy mental illness. I dislike numbers a lot. But it's a little complicated:
+//  The number I'm afraid of depends on which day of the week it is... This is a concrete description of my mental illness:
+
+// Monday --> 12
+
+// Tuesday --> numbers greater than 95
+
+// Wednesday --> 34
+
+// Thursday --> 0
+
+// Friday --> numbers divisible by 2
+
+// Saturday --> 56
+
+// Sunday --> 666 or -666
+
+// Write a function which takes a string (day of the week) and an integer (number to be tested) so it tells the doctor if I'm afraid or not. (return a boolean)
